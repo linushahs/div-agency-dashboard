6722f06713e8b4f6e9082b79c1cb16fb56b263b6
linushahs <suniltraveler2004@gmail.com>
2023-05-14 14:57:51 +0545

    Fixes vertical nav scrollbar issues

--- src/components/VerticalNavbar.tsx
+++ src/components/VerticalNavbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import SimpleBar from "simplebar-react";
 
 export default function VerticalNavbar() {
   const [showContact, setShowContact] = useState(false);
@@ -49,9 +50,8 @@ export default function VerticalNavbar() {
           </span>
         </div>
         {/* <!-- /Brand --> */}
-
         {/* <!-- Main Menu --> */}
-        <div data-simplebar className="nicescroll-bar">
+        <SimpleBar style={{ minHeight: "100%" }}>
           <div className="menu-content-wrap">
             <div className="menu-group">
               <ul className="navbar-nav flex-column">
@@ -691,77 +691,8 @@ export default function VerticalNavbar() {
                 </li>
               </ul>
             </div>
-            <div className="menu-gap"></div>
-            <div className="menu-group">
-              <div className="nav-header">
-                <span>Documentation</span>
-              </div>
-              <ul className="navbar-nav flex-column">
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    href="https://nubra-ui.hencework.com/"
-                    target="_blank"
-                  >
-                    <span className="nav-icon-wrap">
-                      <span className="svg-icon">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-file-code-2"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          strokeWidth="2"
-                          stroke="currentColor"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                          <path d="M10 12h-1v5h1" />
-                          <path d="M14 12h1v5h-1" />
-                          <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-                          <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-                        </svg>
-                      </span>
-                    </span>
-                    <span className="nav-link-text">Documentation</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    href="https://nubra-ui.hencework.com/avatar.html"
-                    target="_blank"
-                  >
-                    <span className="nav-icon-wrap">
-                      <span className="svg-icon">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-layout"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          strokeWidth="2"
-                          stroke="currentColor"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                          <rect x="4" y="4" width="6" height="5" rx="2" />
-                          <rect x="4" y="13" width="6" height="7" rx="2" />
-                          <rect x="14" y="4" width="6" height="16" rx="2" />
-                        </svg>
-                      </span>
-                    </span>
-                    <span className="nav-link-text">Components</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
           </div>
-        </div>
+        </SimpleBar>
         {/* <!-- /Main Menu --> */}
       </div>
       <div id="hk_menu_backdrop" className="hk-menu-backdrop"></div>
